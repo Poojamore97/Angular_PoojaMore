5fbcc83e7c8bd2d047411344c7b0ac7b1b992f1d
Pooja More <pooja7770007@gmail.com>
2020-01-10 00:37:51 +0530

    Added material in angular project.

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +7,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  dashboardData: any
+  setSelectedCategoryBgColor: any;
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+    this.http.get('assets/data.json').subscribe(response => {
+      this.dashboardData = response;
+      console.log(this.dashboardData)
+    })
+  }
 
   ngOnInit() {
+  }
+  selectCategory(data) {
+    this.dashboardData.forEach(element => {
+      element.category.filter(category => {
+        if (category.name === data.name) {
+          this.setSelectedCategoryBgColor = true;
+        } else {
+          this.setSelectedCategoryBgColor = false
+        }
+      })
+    });
+
+    console.log(data)
   }
 
 }
--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HeaderComponent } from './header/header.component';
+import {MatSliderModule} from '@angular/material/slider';
+import { MatFormFieldModule } from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select"
 
 
 @NgModule({
@@ -12,6 +15,9 @@ import { HeaderComponent } from './header/header.component';
     ],
     imports: [
         BrowserModule,
+        MatSliderModule,
+        MatFormFieldModule,
+        MatSelectModule
     ],
     providers: [],
     exports: [HeaderComponent]
